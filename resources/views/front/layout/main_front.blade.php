496578976363c4f6dc8f56feab996ff531b317c8
irvanpir <irvanpir@gmail.com>
2025-06-14 18:13:27 +0700

    update view main front

--- resources/views/front/layout/main_front.blade.php
+++ resources/views/front/layout/main_front.blade.php
@@ -30,7 +30,7 @@
 <!-- Navbar -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-success sticky-top shadow-sm">
   <div class="container">
-    <a class="navbar-brand fw-bold" href="#">SIMRS UNIVERSITAS HANG TUAH PEKANBARU</a>
+    <a class="navbar-brand fw-bold" href="#">MUHAMMAD IRVAN - 24082017(TI)</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
       <span class="navbar-toggler-icon"></span>
     </button>
